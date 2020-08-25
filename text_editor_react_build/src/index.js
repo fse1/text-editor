16961d0dbe0246a4288edb6ee69c5d0d38e8289c
a <26489386+fse1@users.noreply.github.com>
2020-08-25 18:40:33 -0400

    added basic js build pipeline

---
+++ text_editor_react_build/src/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+     {
+      test: "/\.js$/",
+      exclude: "/node_modules/",
+      loader: "babel-loader",
+      options: { presets: ["@babel/env"] }
+     }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "editor-bundle.js"
+  }
+};
---
+++ text_editor_react_build/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+     {
+      test: "/\.js$/",
+      exclude: "/node_modules/",
+      loader: "babel-loader",
+      options: { presets: ["@babel/env"] }
+     }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "editor-bundle.js"
+  }
+};
