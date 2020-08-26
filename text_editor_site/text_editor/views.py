7caddf2a717d80d2253433d7fa4cc0287943129f
a <26489386+fse1@users.noreply.github.com>
2020-08-26 12:54:54 -0400

    added test templates and view

--- text_editor_site/text_editor/views.py
+++ text_editor_site/text_editor/views.py
@@ -5,7 +5,7 @@ from django.templatetags.static import static
 
 def editor(request):
   basic_page = {'header_links': {'css': [], 'js': []}, 'title': 'Text Editor'}
-  react_page = {'id': 'editor-app', 'bottom_js_links': []}
+  react_page = {'id': 'editor-app', 'bottom_js_links': [static('text_editor/editor-bundle.js')]}
   template_context = {'basic_page': basic_page, 'react_page': react_page}
   
   return render(request, 'text_editor/react_app_injection.html', template_context)
